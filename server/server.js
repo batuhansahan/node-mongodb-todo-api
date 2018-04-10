2a6f260759548db2034adeffb342a5733bfea0dc
batuhansahan <batuhansahan@trakya.edu.tr>
2018-04-10 16:26:40 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -30,8 +31,29 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // If id valid check
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    // if todo is empty send 404 and empty object
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
 
 module.exports = {app};
