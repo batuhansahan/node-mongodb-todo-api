c56c801c35fa7502863c6d540b8b725c45e791e0
batuhansahan <batuhansahan@trakya.edu.tr>
2018-04-13 17:30:07 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
-  return _.pick(userObject, ['id', 'email']);
+  return _.pick(userObject, ['_id', 'email']);
 };
 
 UserSchema.methods.generateAuthToken = function () {
@@ -66,6 +66,26 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email,password){
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        }else {
+          reject();
+        }
+      })
+    });
   });
 };
 
